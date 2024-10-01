1581fcb4618f7cb726d4af3c16607a6ea02ac150
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2024-10-01 13:37:37 +0530

    Create Navbar.js

---
+++ secondary/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#upload">Upload Photo</a></li>
+        <li><a href="#leaderboard">Leaderboard</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
