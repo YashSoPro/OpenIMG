e46cdd2c677a91028b68d764cedae906a64ee9d1
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2024-10-10 14:42:04 +0530

    Delete secondary directory

--- secondary/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-const dbConfig = {
-  host: 'sql.freedb.tech',
-  user: 'your_username', // Replace with your MySQL username
-  password: 'your_password', // Replace with your MySQL password
-  database: 'freedb_openimg',
-};
-
-module.exports = dbConfig;
